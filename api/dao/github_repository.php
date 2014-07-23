c7812f00b53bf1f267b6a9698b46a0c5f3f7e4a3
Jeff Standen <jeff@webgroupmedia.com>
2014-07-23 00:31:34 -0700

    [Code Cleanup]

--- api/dao/github_repository.php
+++ api/dao/github_repository.php
@@ -358,12 +358,8 @@ class DAO_GitHubRepository extends Cerb_ORMHelper {
 		$results = array();
 		
 		while($row = mysqli_fetch_assoc($rs)) {
-			$result = array();
-			foreach($row as $f => $v) {
-				$result[$f] = $v;
-			}
 			$object_id = intval($row[SearchFields_GitHubRepository::ID]);
-			$results[$object_id] = $result;
+			$results[$object_id] = $row;
 		}
 
 		$total = count($results);
